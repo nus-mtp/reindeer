8734d5e57ed919898193af45c00a2f2bff47d6c3
hlhr202 <hlhr202@gmail.com>
2015-11-04 14:41:54 +0800

    canvas server test

--- source/socket.js
+++ source/socket.js
@@ -3,28 +3,25 @@ var io = require('socket.io')();
 
 var messageio = io.of('/messagetest');
 
-var listen = function(app) {
-	io.listen(app);
-	console.log('socket listen on ' + app.address().port);
+var listen = function (app) {
+    io.listen(app);
+    console.log('socket listen on ' + app.address().port);
 }
 
-var canvasconnect = function(url){
-	var canvasio;
-	console.log(url);
-	canvasio = io.of(url);
-	canvasio.on('connection', function(socket){
-		console.log('a user connected to '+url);
-		console.log(socket.request.headers);
-		socket.emit('msg',{
-			msg: socket.request.headers,
-		});
-		socket.on('canvasState', function(msg){
-			console.log(msg);
-		});
-	});
+var canvasconnect = function (url) {
+    var canvasio;
+    console.log(url);
+    canvasio = io.of(url);
+    canvasio.on('connection', function (socket) {
+        console.log('a user connected to ' + url);
+        console.log(socket.request.headers);
+        socket.on('canvasState', function (canvas) {
+            canvasio.emit('canvasState', canvas);
+        });
+    });
 }
 
-var messageconnect = function (url){
+var messageconnect = function (url) {
     var messageio;
     console.log(url);
     messageio = io.of(url);
@@ -37,11 +34,11 @@ var messageconnect = function (url){
     });
 }
 
-messageio.on('connection', function(socket){
-	console.log('a user connected to message');
-	socket.emit('msg',{
-		msg: socket.request.headers,
-	});
+messageio.on('connection', function (socket) {
+    console.log('a user connected to message');
+    socket.emit('msg', {
+        msg: socket.request.headers,
+    });
 });
 
 module.exports.listen = listen;
