467658f1d69fb2362b374bc5631be761e10a86b6
look321 <reallookbaoxiao@gmail.com>
2016-02-05 00:52:40 +0800

    Simple UI
    Implemented a simple UI

--- source/controllers/fileUpload.js
+++ source/controllers/fileUpload.js
@@ -6,27 +6,27 @@
 var express = require('express')
 var multer = require('multer')
 
+//var get = function (req, res, next) {
+//	res.send("Hello from the other side...");
+//}
+//
+//var testPost = function (req, res, next) {
+//	res.send(req.body);
+//}
+//
+//var upload = function (req, res, next) {
+//	console.log(req.file);
+//	res.send(req.body);
+//}
+
 var get = function (req, res, next) {
-	res.send("Hello from the other side...");
-}
-
-var testPost = function (req, res, next) {
-	res.send(req.body);
-}
-
-var upload = function (req, res, next) {
-	console.log(req.file);
-	res.send(req.body);
-}
-
-var getTestPage = function (req, res, next) {
-	res.render('testAJAX', {
+	res.render('fileUpload', {
 		title: 'File Upload',
 		ip: req.app.get('server-ip')
 	});
 }
 
 module.exports.get = get;
-module.exports.upload = upload;
-module.exports.getTestPage = getTestPage;
-module.exports.testPost = testPost;
+//module.exports.upload = upload;
+//module.exports.getTestPage = getTestPage;
+//module.exports.testPost = testPost;
--- source/router.js
+++ source/router.js
@@ -5,10 +5,20 @@
 var express = require('express');
 var multer = require('multer');
 var router = express.Router();
-var upload = multer({
-	dest: '../public/uploads/',
-});
+//var upload = multer({
+//	dest: '../public/uploads/',
+//	filename: '1.jpg',
+//});
 
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, '../public/uploads/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname)
+	}
+})
+var upload = multer({ storage: storage })
 
 var index = require('./controllers/index');
 var tutorial = require('./controllers/tutorial');
@@ -27,7 +37,6 @@ router.get('/messagetest', message.get);
 router.get('/messagetest/:id', message.get);
 
 router.get('/fileUpload', fileUpload.get);
-router.get('/testAJAX', fileUpload.getTestPage);
 router.post('/fileupload', upload.single('photo'), function (req, res, next){});
 router.post('/api/tutorial/createroom', tutorial.createRoom);
 router.post('/api/tutorial/roomparams', tutorial.roomParams);
