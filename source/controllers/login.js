47d7b29314f626bb474297e032507c0ca105e0ac
hlhr202 <hlhr202@163.com>
2016-02-17 22:41:25 +0800

    ivle module retrieve when login

--- source/controllers/login.js
+++ source/controllers/login.js
@@ -3,6 +3,8 @@ var User = require ('../models/user');
 var rest = require ('rest');
 var auth = require ('../auth');
 var app = require ('../../app');
+var Tutorial = require('../models/tutorial');
+
 var protocol = 'https';
 var usehttps = app.get ('use-https');
 if (!usehttps) {
@@ -13,6 +15,7 @@ var get = function (req, res, next) {
 	if (auth.success) {
 		res.redirect ('/');
 	}
+	//user login and callback
 	else res.redirect ('https://ivle.nus.edu.sg/api/login/?apikey=dQ52oB9BDUvIKSsyntdtW&url='+protocol+'://' + app.get ('server-ip') + ':' + app.get ('server-port') + '/login/callback');
 }
 
@@ -21,10 +24,29 @@ var callback = function (req, res, next) {
 	var apikey = app.get ('api-key');
 	console.log (apikey);
 
+	//view profile
 	rest ('https://ivle.nus.edu.sg/api/Lapi.svc/Profile_View?APIKey=' + apikey + '&AuthToken=' + token).then (function (response) {
 		var result = JSON.parse (response.entity).Results[0];
 		if (result != undefined) {
 			result.Token = token;
+
+			//view user modules
+			rest('https://ivle.nus.edu.sg/api/Lapi.svc/Modules?APIKey=' + apikey + '&AuthToken=' + token+'&Duration=0&IncludeAllInfo=false').then(function(response){
+				var courses = JSON.parse(response.entity).Results;
+				for(idx in courses){
+					var isManager = courses[idx]['Permission'];
+					if (isManager === 'M'){
+						var courseid = courses[idx]['ID'];
+						var acadyear = courses[idx]['AcadYear'];
+						var semester = courses[idx]['Semester'];
+						//view tutorial groups
+						rest('https://ivle.nus.edu.sg/API/Lapi.svc/GroupsByUserAndModule?APIKey='+apikey+'&AuthToken='+token+'&CourseID='+courseid+'&AcadYear='+acadyear+'&Semester='+semester).then(function(response){
+							console.log(JSON.parse(response.entity));
+
+						});
+					}
+				}
+			});
 			User.findOrCreate ({
 				where: {
 					id: result.UserID
--- source/models/tutorial.js
+++ source/models/tutorial.js
@@ -1,16 +1,30 @@
 var express = require('express');
-var sequelize = require('./sequelize');
+var sequelize = require('../sequelize');
 var Sequelize = require('sequelize');
+var User = require('./user');
 
-var tutorial = sequelize.define('tutorial',{
-	id: {
-		type: Sequelize.INTEGER,
-		autoIncrement: true,
-		primarykey: true
+var tutorial = sequelize.define('tutorial', {
+	name: {
+		type: Sequelize.STRING,
+		unique:'compositeIndex'
+	},
+	courseid:{
+		type:Sequelize.STRING,
+		unique:'compositeIndex'
+	},
+	coursecode:{
+		type:Sequelize.STRING
+	},
+	coursename:{
+		type:Sequelize.STRING
 	},
 	datetime:{
-		type: Sequelize.STRING,
-	},
+		type: Sequelize.STRING
+	}
+},{
 
+});
 
-})
+sequelize.sync();
+
+module.exports = tutorial;
