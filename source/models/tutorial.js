060d6033bf506c413641dbfedf3943b2ff353f72
hlhr202 <hlhr202@163.com>
2016-02-18 07:39:55 +0800

    Update login, migrating tutorial info when user login, use uuid for tutorial id, add user tutorial n:m association with half constraints (only tutorial id has been set as foreign key)

--- source/models/tutorial.js
+++ source/models/tutorial.js
@@ -1,30 +1,92 @@
-var express = require('express');
-var sequelize = require('../sequelize');
-var Sequelize = require('sequelize');
-var User = require('./user');
+var express = require ('express');
+var sequelize = require ('../sequelize');
+var Sequelize = require ('sequelize');
+var User = require ('./user');
 
-var tutorial = sequelize.define('tutorial', {
+var tutorial = sequelize.define ('tutorial', {
+	id: {
+		type: Sequelize.UUID,
+		defaultValue: Sequelize.UUIDV4,
+		unique: true,
+		primaryKey: true
+	},
+	grouptype: {
+		type: Sequelize.STRING,
+	},
 	name: {
 		type: Sequelize.STRING,
-		unique:'compositeIndex'
 	},
-	courseid:{
-		type:Sequelize.STRING,
-		unique:'compositeIndex'
+	courseid: {
+		type: Sequelize.STRING,
 	},
-	coursecode:{
-		type:Sequelize.STRING
+	coursecode: {
+		type: Sequelize.STRING
 	},
-	coursename:{
-		type:Sequelize.STRING
+	coursename: {
+		type: Sequelize.STRING
 	},
-	datetime:{
+	week: {
+		type: Sequelize.STRING
+	},
+	day: {
+		type: Sequelize.STRING
+	},
+	time: {
 		type: Sequelize.STRING
 	}
-},{
-
+}, {
+	indexes: [
+		{
+			name:'name',
+			fields: ['name']
+		},
+		{
+			name:'courseid',
+			fields: ['courseid']
+		}
+	]
 });
 
-sequelize.sync();
+var userTutorial = sequelize.define ('userTutorial', {
+	role: {
+		type: Sequelize.ENUM,
+		values: ['student', 'tutor'],
+		allowNull: false
+	},
+	tutorialId: {
+		type: Sequelize.UUID,
+		references: {
+			model: tutorial,
+			key: 'id'
+		}
+	}
+}, {
+	indexes: [
+		{
+			name:'tutorialId',
+			fields: ['tutorialId']
+		},
+		{
+			name:'userId',
+			fields: ['userId']
+		}
+	]
+})
+
+User.belongsToMany (tutorial, {
+	constraints: false,
+	foreignKey: 'userId',
+	through: 'userTutorial',
+	as: 'tutorial'
+});
+
+tutorial.belongsToMany (User, {
+	constraints: false,
+	foreignKey: 'tutorialId',
+	through: 'userTutorial',
+	as: 'user'
+})
+
+sequelize.sync ();
 
 module.exports = tutorial;
