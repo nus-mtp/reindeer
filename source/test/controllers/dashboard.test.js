bf0fbc97a19f517526113bedc89705cdba723269
Chen Di <sundychenya1018@yahoo.com>
2016-02-22 00:41:20 +0800

    Fix test bug, update router

--- source/test/controllers/dashboard.test.js
+++ source/test/controllers/dashboard.test.js
@@ -7,12 +7,12 @@ var expect = chai.expect;
 
 var test = function () {
 	describe ("Dashboard Restful API", function () {
-		describe ("#getAllUserTutorial", function () {
+		describe ("#getAllUserTutorialSessions", function () {
 			this.timeout(15000);
 			before (function (done) {
 				httpUtils._save ({
 					method: 'POST',
-					url: 'http://localhost:3000/api/dashboard/getallusertutorials',
+					url: 'http://localhost:3000/api/dashboard/getAllUserTutorialSessions',
 					form: {
 						"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImEwMDkxNzM4IiwibmFtZSI6IkhVQU5HIExJVUhBT1JBTiIsImlhdCI6MTQ1NTc5ODQ2NCwiZXhwIjoxNDU4MzkwNDY0fQ.EiZsG9bn2S3hB4jL20uJ-h1YVIsQ17xDO1z7o2GrqLs"
 					}
