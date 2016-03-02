ce68003bffbd6ec16c722128f784b55d9e6f6d8e
Shi Yu Phan <phanshiyu@gmail.com>
2016-03-02 21:21:56 +0800

    Added models to support the PresentationController

---
+++ source/models/Slide.js
@@ -0,0 +1,12 @@
+/**
+ * Created by shiyu on 2/3/16.
+ */
+var ActionStackToUserManager = require('.ActionStackToUserManager');
+
+var Slide = function(slideImagePath) {
+    var actionStackToUserManager = new ActionStackToUserManager();
+    var slideImagePath = '';
+}
+
+module.exports = Slide;
+
