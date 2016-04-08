ea762b6318abb713c58c688e00b1d463cb0bec21
Chen Di <sundychenya1018@yahoo.com>
2016-04-08 18:57:40 +0800

    Extend timeout tie

--- source/test/controllers/tutorial.test.js
+++ source/test/controllers/tutorial.test.js
@@ -12,7 +12,7 @@ var expect = chai.expect;
 var test = function () {
 	describe ("Tutorial Restful API", function () {
 		describe ("#activateAndCreateRoom", function () {
-			this.timeout(15000);
+			this.timeout(25000);
 			before (function (done) {
 				httpUtils._save({
 					method: 'POST',
