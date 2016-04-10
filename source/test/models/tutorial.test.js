b14685e51ecf52699693e4a29fb7ee671263ca88
Chen Di <sundychenya1018@yahoo.com>
2016-04-10 11:03:09 +0800

    Add tutorial model test case

--- source/test/models/tutorial.test.js
+++ source/test/models/tutorial.test.js
@@ -11,16 +11,60 @@ var expect = chai.expect;
 
 var test = function () {
 	describe ('Tutorial Model', function () {
-		describe ('#findAndCountAllTutorialByUserID()', function () {
+
+		describe ('#findTutorial()', function () {
+			this.timeout (25000);
+			it ('should sync data from ivle', function () {
+				var findTut = Tutorial.findTutorial('a0119493', 'testid');
+
+				return findTut.then (function (result) {
+					(result).should.not.equal (undefined);
+				});
+			})
+		});
+
+		describe ('#findTutorialTutorID()', function () {
+			this.timeout (25000);
+			it ('should sync data from ivle', function () {
+				var findTutTutorID = Tutorial.findTutorialTutorID('testid');
+
+				return findTutTutorID.then (function (result) {
+					(result).should.not.equal (undefined);
+				});
+			})
+		});
+
+		describe ('#checkIfInTutorialUserList()', function () {
+			this.timeout (25000);
+			it ('should sync data from ivle', function () {
+				var checkIfInTutorialUserList = Tutorial.checkIfInTutorialUserList('a0119493', 'testid');
+
+				return checkIfInTutorialUserList.then (function (result) {
+					(result).should.not.equal (undefined);
+				});
+			})
+		});
+
+		describe ('#findAndCountAllTutorials()', function () {
 			this.timeout (25000);
 			it ('should retrive data from database', function () {
 				var queryDatabase = Tutorial.findAndCountAllTutorials ('a0119493');
 
 				return queryDatabase.then (function (result) {
-					expect (result).should.not.equal (undefined);
+					(result).should.not.equal (undefined);
 				});
 			});
+		});
 
+		describe ('#findAndCountAllUsersInTutorial()', function () {
+			this.timeout (25000);
+			it ('should retrive data from database', function () {
+				var queryDatabase = Tutorial.findAndCountAllUsersInTutorial ('testid');
+
+				return queryDatabase.then (function (result) {
+					(result).should.not.equal (undefined);
+				});
+			});
 		});
 
 		describe ('#forceSyncIVLE()', function () {
@@ -29,10 +73,21 @@ var test = function () {
 				var syncIVLE = Tutorial.forceSyncIVLE ('a0119493');
 
 				return syncIVLE.then (function (result) {
-					expect (result).to.equal (true);
+					(result).should.equal (true);
 				});
 			})
-		})
+		});
+
+		describe ('#findTutorialSession()', function () {
+			this.timeout (25000);
+			it ('should retrive data from database', function () {
+				var queryDatabase = Tutorial.findTutorialSession ('a0119493');
+
+				return queryDatabase.then (function (result) {
+					(result).should.not.equal (undefined);
+				});
+			});
+		});
 
 		//clean up after all test
 		after (function () {
